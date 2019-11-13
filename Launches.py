af2c5fb659645c1be2162e3f4ebc60c1af7f20a6
SchillingW <46679070+SchillingW@users.noreply.github.com>
2019-11-13 07:14:10 -0500

    Update Launches.py

--- Launches.py
+++ Launches.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env python3
 L1 = [
-    ["Drive", 10, 100]
-]
-
-L2 = [
-    ["DriveUltrasonic", 20, 100]
-]
-
-L3 = [
+    ["LineFollow", 10],
     ["Turn", 10, 18, 180]
 ]
 
+L2 = [
+]
+
+L3 = [
+]
+
 L4 = [
-    ["MotorOn", 1, 1],
-    ["Wait", 2],
-    ["MotorOn", 0, -1],
-    ["Wait", 3],
-    ["MotorOff", 0],
-    ["MotorOff", 1]
 ]
 
 Launches = [L1, L2, L3, L4]
