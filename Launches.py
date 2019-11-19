16f57f6c6088c8ddeeb5ce034319aaa12713dec4
SchillingW <46679070+SchillingW@users.noreply.github.com>
2019-11-19 18:11:17 -0500

    Update Launches.py

--- Launches.py
+++ Launches.py
@@ -31,28 +31,38 @@
 #   ["Wait"            ,              4  ],
 # ]
 
-L1 = [
-    ["Drive"           ,  0  ,  5  ,  5  ],
-    ["DriveUltrasonic" ,  0  ,  5  ,  60 ],
-    ["DriveUltrasonic" ,  0  , -20 ,  20 ],
-    ["Turn"            , -20 ,  30 ,  90 ],
+LargeTower = [
+    ["Drive"           ,  0  ,  5   ,  5   ],
+    ["DriveUltrasonic" ,  0  ,  5   ,  60  ],
+    ["DriveUltrasonic" ,  0  , -20  ,  20  ],
+    ["Turn"            , -20 ,  30  ,  90  ],
 ]
 
-L2 = [
-    ["Turn"            ,  13 ,  30 ,  90 ],
-    ["Drive"           ,  90 ,  10 ,  90 ],
-    ["Turn"            ,  0  , -30 ,  45 ],
-    ["LineFollow"      ,        5        ],
-    ["Turn"            , -5  ,  30 ,  90 ],
-    ["Drive"           ,  90 ,  10 ,  15 ],
-    ["DriveUltrasonic" ,  90 , -20 ,  15 ],
-    ["Turn"            ,  15 , -30 ,  0  ],
+South = [
+    ["Turn"            ,  10 ,  30  ,  90  ],
+    ["MotorOn"         ,  1  ,  .06        ],
+    ["Drive"           ,  90 ,  10  ,  90  ],
+    ["MotorOff"        ,  1                ],
+    ["Turn"            ,  0  , -30  ,  60  ],
+    ["LineFollow"      ,        5          ],
+    ["Turn"            , -5  ,  30  ,  90  ],
+    ["Drive"           ,  90 ,  10  ,  15  ],
+    ["DriveUltrasonic" ,  90 , -20  ,  16  ],
+    ["Turn"            ,  15 , -30  ,  0   ],
 ]
 
-L3 = [
+RedTower = [
+    ["Drive"           ,  0  ,  10  ,  103 ],
+    ["Drive"           ,  0  , -10  , -103 ],
 ]
 
-L4 = [
+TanTower = [
+    ["Drive"           ,  0  ,  10  ,  65  ],
+    ["Drive"           ,  0  , -10  , -65  ],
 ]
 
-Launches = [L1, L2, L3, L4]
+Bridge = [
+    ["Drive"           ,  0  ,  50  ,  65  ],
+]
+
+Launches = [LargeTower, South, RedTower, TanTower, Bridge]
